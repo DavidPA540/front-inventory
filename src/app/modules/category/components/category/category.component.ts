9463a0d24718204f98b95853f5b2ed3a4f3c657c
David Palacios Acosta <david_palacios.acosta@hotmail.com>
2022-07-19 12:56:06 -0500

    paginador

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -23,6 +23,9 @@ export class CategoryComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
 
+  @ViewChild(MatPaginator)
+  paginator !: MatPaginator;
+
   getCategories(){
 
     this.categoryService.getCategories()
@@ -49,6 +52,8 @@ export class CategoryComponent implements OnInit {
       });
 
       this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+      this.dataSource.paginator = this.paginator;
+
 
     }
 
