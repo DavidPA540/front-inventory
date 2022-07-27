0c1354bf5903769a7407135edf0d92fd63002c46
David Palacios Acosta <david_palacios.acosta@hotmail.com>
2022-07-27 08:55:53 -0500

    Graficas

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
 import { CategoryModule } from '../category/category.module';
 import { ProductModule } from '../product/product.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgChartsModule } from 'ng2-charts';
 
 
 
@@ -18,7 +20,10 @@ import { ProductModule } from '../product/product.module';
     CommonModule,
     SharedModule,
     CategoryModule,
-    ProductModule
+    ProductModule,
+    MaterialModule,
+    FlexLayoutModule,
+    NgChartsModule
   ]
 })
 export class DashboardModule { }
